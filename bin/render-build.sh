192dedde215fc025370d75cfe6ace9bc1140a4da
Diego Silva Rodríguez <129236136+DiegoSilvaR@users.noreply.github.com>
2023-07-11 08:40:30 -0400

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 
+bundle exec rake db:drop
+bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rails db:seed
